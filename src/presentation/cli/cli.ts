efd12106641f9c64c2ecee01d341d98c136bf38f
Yuku Kotani <yukukotani@gmail.com>
2025-08-11 02:03:10 +0900

    sub command

--- src/presentation/cli/cli.ts
+++ src/presentation/cli/cli.ts
@@ -3,7 +3,10 @@ import { setLogLevel } from "../../libs/logger.js";
 import { readCommand } from "./read-command.js";
 
 export async function execute() {
+  const subCommands = new Map([["read", readCommand]]);
+
   await cli(process.argv.slice(2), readCommand, {
+    subCommands,
     onBeforeCommand(ctx) {
       if (ctx.values.logLevel) {
         setLogLevel(ctx.values.logLevel);
